9c2f575d3649810f7ae451b94fa1510d4b634666
Rogier Roex <rogier.roex@gmail.com>
2024-05-08 22:18:16 +0200

    extra tiles

--- src/main/java/nl/rrx/tile/TileType.java
+++ src/main/java/nl/rrx/tile/TileType.java
@@ -1,11 +1,14 @@
 package nl.rrx.tile;
 
 public enum TileType {
-    DESERT("/images/tiles/desert.png"),
+    GRASS("/images/tiles/grass.png"),
     WALL("/images/tiles/wall.png"),
-    WATER("/images/tiles/water.png");
+    WATER("/images/tiles/water.png"),
+    DIRT("/images/tiles/dirt.png"),
+    DESERT("/images/tiles/desert.png"),
+    TREE("/images/tiles/tree.png");
 
-    private String imageUri;
+    private final String imageUri;
 
     TileType(String imageUri) {
         this.imageUri = imageUri;
