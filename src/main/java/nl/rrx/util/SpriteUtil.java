badb31db6b6756c18f8222befb7363b2f9e23d3f
Rogier Roex <rogier.roex@gmail.com>
2024-05-28 23:47:19 +0200

    tweeks

--- src/main/java/nl/rrx/util/SpriteUtil.java
+++ src/main/java/nl/rrx/util/SpriteUtil.java
@@ -5,17 +5,24 @@ import static nl.rrx.config.settings.SpriteSettings.SPRITE_REFRESH_RATE;
 public class SpriteUtil {
 
     private int spriteCounter = 0;
-    private int spriteNum = 1;
+    private int spriteNum = 0;
+    private boolean isAlreadyUpdatedDuringFrame; // do not update twice when holding UP|DOWN & LEFT|RIGHT at the same time
 
     public void updateSprite() {
+        if (isAlreadyUpdatedDuringFrame) {
+            return;
+        }
+        isAlreadyUpdatedDuringFrame = true;
+
         spriteCounter++;
         if (spriteCounter > SPRITE_REFRESH_RATE) {
-            spriteNum = (spriteNum != 1) ? 1 : 2;
+            spriteNum = (spriteNum == 1) ? 0 : 1;
             spriteCounter = 0;
         }
     }
 
     public boolean isNewDirection() {
+        this.isAlreadyUpdatedDuringFrame = false;
         return spriteNum == 1;
     }
 }
